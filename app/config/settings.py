3a347762375b5e400b91220cf711ad1ac4ce0081
mingyu <rlaalsrbgk@gmail.com>
2018-07-05 14:42:45 +0900

    bootstrap 및 이미지 적용, view와 url연결, static 및 templates setting.py에 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -33,6 +35,11 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -58,7 +65,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
